e0a9c163f3f13f0323e3a2bd6b1f6ad01d4b4447
Archy Fut <futsmit@yandex.ru>
2023-09-08 12:23:08 +0300

    Added routing, lazy loading pages and two themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import {Counter} from "./Components/Counter";
-import './index.scss';
+import React, {Suspense, useContext, useState} from 'react';
+import {Counter} from "./components/Counter";
+import './styles/index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
+import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, changeTheme}=useTheme();
     return (
-        <div className="app">
-            erheherherh
-            <Counter />
+        <div className={`app ${theme}`}>
+            <button onClick={changeTheme}>TOGGLE THEME</button>
+            <Link to={'/'}>Главная страница</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading....</div>}>
+                <Routes>
+                    <Route path="/about" element={<AboutPageLazy />}/>,
+                    <Route path="/" element={<MainPageLazy />} />
+                </Routes>
+            </Suspense>
+
         </div>
     );
 };
