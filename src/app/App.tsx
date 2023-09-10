13dcb111c9f21a2ae2058a0dcab2e9b26ccb895e
Archy Fut <futsmit@yandex.ru>
2023-09-10 20:52:15 +0300

    Added fsd architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,22 @@
-import React, {Suspense, useContext, useState} from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
 import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
-import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
-import {useTheme} from "./theme/useTheme";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, changeTheme}=useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app',{},[theme])}>
             <button onClick={changeTheme}>TOGGLE THEME</button>
             <Link to={'/'}>Главная страница</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading....</div>}>
                 <Routes>
-                    <Route path="/about" element={<AboutPageLazy />}/>,
-                    <Route path="/" element={<MainPageLazy />} />
+                    <Route path="/about" element={<AboutPage />}/>,
+                    <Route path="/" element={<MainPage />} />
                 </Routes>
             </Suspense>
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -5,9 +5,10 @@ import path from "path";
 
 export default(env:BuildEnv)=>{
     const paths:BuildPaths={
-    entry:path.resolve(__dirname,'src', 'index.tsx'),
-    build:path.resolve(__dirname, 'build'),
-    html:path.resolve(__dirname,'public', 'index.html')
+        entry:path.resolve(__dirname,'src', 'index.tsx'),
+        build:path.resolve(__dirname, 'build'),
+        html:path.resolve(__dirname,'public', 'index.html'),
+        src:path.resolve(__dirname,'src')
 }
 
 const mode=env.mode||'development';
