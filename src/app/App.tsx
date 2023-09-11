847e846158cb6d081a8ce42e468f1f161a3d2cc9
Archy Fut <futsmit@yandex.ru>
2023-09-11 15:44:05 +0300

    Added App router and config router. Added navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,18 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {NavBar} from "widgets/NavBar";
 
 const App = () => {
     const {theme, changeTheme}=useTheme();
     return (
         <div className={classNames('app',{},[theme])}>
+            <NavBar/>
+            <AppRouter/>
             <button onClick={changeTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Главная страница</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading....</div>}>
-                <Routes>
-                    <Route path="/about" element={<AboutPage />}/>,
-                    <Route path="/" element={<MainPage />} />
-                </Routes>
-            </Suspense>
-
         </div>
     );
 };
