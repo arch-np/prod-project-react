3367644863f2ad26bb022d49bc71d515d2a4b6fc
Archy Fut <futsmit@yandex.ru>
2023-09-13 18:25:16 +0300

    Added:  - i18n  - Webpack hot module replacement  - babel

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss';
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
@@ -7,14 +7,17 @@ import {NavBar} from "widgets/NavBar";
 import {Sidebar} from "widgets/Sidebar";
 
 const App = () => {
+
     const {theme}=useTheme();
     return (
         <div className={classNames('app',{},[theme])}>
-            <NavBar/>
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback="">
+                <NavBar/>
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import cls from './Sidebar.module.scss';
 import {useState} from "react";
 import {Button} from "shared/ui/Button/Button";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
 
 
 interface SidebarProps {
@@ -23,6 +24,7 @@ export const Sidebar = ({className}: SidebarProps) => {
           </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang} />
             </div>
 
         </div>
