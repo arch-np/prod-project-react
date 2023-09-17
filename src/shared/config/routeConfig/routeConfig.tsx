a1539f1711643f117649349da452f352c4920cd9
Archy Fut <futsmit@yandex.ru>
2023-09-17 14:27:28 +0300

    Added Not found page

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,25 +1,33 @@
 import {RouteProps} from 'react-router-dom';
 import {MainPage} from 'pages/MainPage';
 import {AboutPage} from 'pages/AboutPage';
+import {NotFoundPage} from 'pages/NotFoundPage';
 
 export enum AppRoutes {
     MAIN='main',
-    ABOUT='about'
+    ABOUT='about',
+    NOT_FOUND='not_found',
 }
 
 export const RoutesPath:Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    // Последний маршрут
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig:Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
-        path: RoutesPath[AppRoutes.MAIN],
+        path: RoutesPath.main,
         element: <MainPage />,
     },
     [AppRoutes.ABOUT]: {
-        path: RoutesPath[AppRoutes.ABOUT],
+        path: RoutesPath.about,
         element: <AboutPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutesPath.not_found,
+        element: <NotFoundPage />,
     },
 
 };
