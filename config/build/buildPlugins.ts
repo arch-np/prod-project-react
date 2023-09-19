e6fdb44a5ee0f2005be6270ad11dd2e98cde7b1c
Archy Fut <futsmit@yandex.ru>
2023-09-19 11:42:47 +0300

    Added Error boundary and bundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,8 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
-import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { BuildOptions } from './types/config';
 export function buildPlugins({ paths, isDev }:BuildOptions):webpack.WebpackPluginInstance[] {
     return [
         new HtmlWebpackPlugin({
@@ -17,5 +17,8 @@ export function buildPlugins({ paths, isDev }:BuildOptions):webpack.WebpackPlugi
             __IS_DEV__: JSON.stringify(isDev),
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ];
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
@@ -8,7 +8,11 @@ import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
     const { theme } = useTheme();
-
+    // UseEffect(() => {
+    //     if (Math.random() < 0.5) {
+    //         throw new Error('Error');
+    //     }
+    // }, []);
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback=''>
