754e7eb54b795267bcb546ce6ecbc4040706671a
Archy Fut <futsmit@yandex.ru>
2023-10-06 08:00:19 +0300

    Added step checks

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,7 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 export function buildPlugins({ paths, isDev }:BuildOptions):webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -21,4 +21,13 @@ export function buildPlugins({ paths, isDev }:BuildOptions):webpack.WebpackPlugi
             openAnalyzer: false,
         }),
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+
+    return plugins;
 }
