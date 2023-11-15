353e4df7da95d3612a4f581a650c8d682532fb16
Archy Fut <futsmit@yandex.ru>
2023-11-15 09:48:09 +0300

    Update app router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,21 +3,25 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { NavBar } from 'widgets/NavBar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
 
 const App = () => {
     const dispatch = useDispatch();
+
+    const inited = useSelector(getUserInited);
+
     useEffect(() => {
         dispatch(userActions.initAuthData());
     }, [dispatch]);
+
     return (
         <div className={classNames('app', {}, [])}>
             <Suspense fallback=''>
                 <NavBar/>
                 <div className='content-page'>
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
