3847cc5fe814d3227cee9c96eb1f76536b494ac1
Archy Fut <futsmit@yandex.ru>
2024-01-25 23:18:54 +0300

    Added html reporter from uint tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -62,7 +62,15 @@ const config: Config = {
         // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
-
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
