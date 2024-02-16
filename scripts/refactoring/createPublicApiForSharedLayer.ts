2dfdbaf9422ee1c3d3c390ef636708528cc69abb
Archy Fut <futsmit@yandex.ru>
2024-02-16 22:12:52 +0300

    improved index api shared layer script

--- scripts/refactoring/createPublicApiForSharedLayer.ts
+++ scripts/refactoring/createPublicApiForSharedLayer.ts
@@ -1,55 +1,67 @@
-import { Project } from 'ts-morph';
 import path from 'path';
+import { Project } from 'ts-morph';
+
+const PROJECT_LAYERS = ['app', 'entities', 'features', 'shared', 'pages', 'widgets'];
 
 const project = new Project({});
-
 project.addSourceFilesAtPaths('src/**/*.ts');
 project.addSourceFilesAtPaths('src/**/*.tsx');
 
-const files = project.getSourceFiles();
-const sharedLayerDirectory = project.getDirectory(
-    path.resolve(__dirname, '..', '..', 'src', 'shared', 'ui'),
-);
-const componentDirs = sharedLayerDirectory?.getDirectories();
-
-function isAbsolute(value: string) {
-    const layers = ['app', 'shared', 'entities', 'features', 'widgets', 'pages'];
-    return layers.some(layer => value.startsWith(layer));
+function isAbsolute(path: string) {
+    return PROJECT_LAYERS.some(layer => path.startsWith(layer));
 }
 
-componentDirs?.forEach(dir => {
-    // console.log(dir.getBaseName());
-    const indexFilePath = dir.getPath() + '/index.ts';
-    const indexFile = dir.getSourceFile(indexFilePath);
-    // console.log(indexFile?.getBaseName());
-    if (!indexFile) {
-        const sourceCode = `export * from './${dir.getBaseName()}'`;
-        const file = dir.createSourceFile(
-            indexFilePath,
-            sourceCode,
+const files = project.getSourceFiles();
+const indexFilename = 'index.ts';
+const layer = process.argv[2] || 'shared';
+const slice = 'ui';
+const dest = project.getDirectory(path.resolve(__dirname, '..', '..', 'src', layer, slice));
+const directories = dest?.getDirectories();
+
+directories?.forEach(directory => {
+    const folderName = directory.getPath();
+    const isIndexFileExist = directory.getSourceFile(`${folderName}/${indexFilename}`);
+
+    if (!isIndexFileExist) {
+        const filesInFolder = directory.getSourceFiles([
+            '**/*.tsx',
+            '!**/*.stories.tsx',
+            '!**/*.test.tsx',
+        ]);
+
+        let content = '';
+
+        filesInFolder?.forEach(component => {
+            const folderLen = folderName.length;
+            const moduleName = component.getBaseNameWithoutExtension();
+            const modulePath = `.${component.getFilePath().slice(folderLen, -4)}`;
+            content += `export {${moduleName}} from "${modulePath}"\n`;
+        });
+        // console.log(content)
+        const file = directory.createSourceFile(
+            `${folderName}/${indexFilename}`,
+            content,
             { overwrite: true },
         );
-        file.save();
+
+        file.save().then(() => console.log(`${folderName} --> index.ts created!`));
     }
 });
 
-files.forEach(sourceFile => {
-    const importDeclarations = sourceFile.getImportDeclarations();
+files.forEach(source => {
+    const declarations = source.getImportDeclarations();
+    declarations.forEach(declaration => {
+        let value = declaration.getModuleSpecifierValue();
+        value = value.replace('@/', '');
+        const segments = value.split('/');
 
-    importDeclarations.forEach(importDeclaration => {
-        const value = importDeclaration.getModuleSpecifierValue();
-        const valueWithoutAlias = value.replace('@/', '');
+        const isLayer = segments?.[0] === layer;
+        const isSlice = segments?.[1] === slice;
 
-        const segments = valueWithoutAlias.split('/');
-
-        const isSharedLayer = segments?.[0] === 'shared';
-        const isUiLayer = segments?.[1] === 'ui';
-
-        if (isAbsolute(valueWithoutAlias) && isSharedLayer && isUiLayer) {
-            const result = valueWithoutAlias.split('/').slice(0, 3).join('/');
-            importDeclaration.setModuleSpecifier(`@/${result}`);
+        if (isAbsolute(value) && isLayer && isSlice) {
+            const res = value.split('/').slice(0, 3).join('/');
+            declaration.setModuleSpecifier(`@/${res}`);
         }
     });
 });
-
-project.save();
+project.save().then(() => console.log('it`s done!'));
