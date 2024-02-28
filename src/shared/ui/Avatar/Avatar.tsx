871636ee23ab68b3d34d82081867cf635d82d908
Archy Fut <futsmit@yandex.ru>
2024-02-28 12:24:26 +0300

    add appImage, fallback and errorFallback

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,5 +1,10 @@
 import { CSSProperties, memo, useMemo } from 'react';
 
+import { AppImage } from '../AppImage';
+import { Icon } from '../Icon';
+import { Skeleton } from '../Skeleton';
+
+import UserIcon from '@/shared/assets/icons/user-filled.svg';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 
 import cls from './Avatar.module.scss';
@@ -8,16 +13,23 @@ interface AvatarProps {
     src?: string;
     size?: number;
     alt?: string;
+    fallbackInverted?:boolean
 }
 
-export const Avatar = memo(({ className, src, size, alt }: AvatarProps) => {
+export const Avatar = memo(({ className, src, size = 100, alt, fallbackInverted }: AvatarProps) => {
     const mods:Mods = {};
     const styles = useMemo<CSSProperties>(() => ({
-        width: size || 100,
-        height: size || 100,
+        width: size,
+        height: size,
     }), [size]);
+
+    const errorFallback = <Icon inverted={fallbackInverted} width={size} height={size} Svg={UserIcon}/>;
+    const fallback = <Skeleton width={size} height={size} border={'50%'} />;
+
     return (
-        <img
+        <AppImage
+            fallback={fallback}
+            errorFallback={errorFallback}
             src={src}
             alt={alt}
             style={styles}
